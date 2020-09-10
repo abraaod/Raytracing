9bf5aa6b0aa60f4368eb551f91f16d1c4ca347d0
Confuzo <abraaovld@gmail.com>
2020-09-10 10:42:52 -0300

    Background class partially implemented

---
+++ include/background.hpp
@@ -0,0 +1,22 @@
+#ifndef _BACKGROUND_
+#define _BACKGROUND_
+
+class Background
+{
+private:
+    /* data */
+
+    std::string type;
+    
+public:
+    Background(/* args */);
+    ~Background();
+};
+
+Background::Background(/* args */)
+{
+}
+
+Background::~Background() = default;
+
+#endif
