8c248e9b62e5c303f9b680ab6eb7a0f891a5d972
Henrique David <henriquemed101@gmail.com>
2020-09-10 16:07:32 -0300

    Adiçao da representaçao de cores, vetores, ...

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,19 @@
 #include "../include/parser.hpp"
 #include <map>
 #include <string>
+#include "../include/vec.hpp"
+
 
 int main(int argc, char const *argv[])
 {
+    // if(argc != 2){
+    //     std::cout << "Ops, informe o arquivo de entrada.\nEX: ./main <inputfile>.xml\n";
+    //     return 0;
+    // }
+
+    // std::cout << "Input file: " << argv[1] << std::endl;
+    // std::string path = argv[1];
+
     std::string path = "./scene/scene.xml";
 
     Parser parser(path);
