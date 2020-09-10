cc13df40165e5f484c6d45ec30d1e2a6262f8a54
Confuzo <abraaovld@gmail.com>
2020-09-10 10:34:00 -0300

    Adding camera.hpp

---
+++ include/camera.hpp
@@ -0,0 +1,24 @@
+#ifndef _CAMERA_
+#define _CAMERA_
+
+#include <string>
+
+class Camera
+{
+private:
+    /* data */
+    std::string type;
+
+public:
+    Camera(std::string type);
+    ~Camera();
+};
+
+Camera::Camera(std::string type)
+{
+    this->type = type;
+}
+
+Camera::~Camera() = default;
+
+#endif
