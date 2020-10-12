45907ec99e2798baad329a44e0fd744efd644d31
Confuzo <abraaovld@gmail.com>
2020-10-12 10:52:28 -0300

    NormalIntegrator implemented

--- include/integrator.hpp
+++ include/integrator.hpp
@@ -38,4 +38,32 @@ class FlatIntegrator : public Integrator{
         virtual ~FlatIntegrator() = default;
 };
 
+class NormalIntegrator : public Integrator{
+    public :
+        NormalIntegrator(std::string type) : Integrator(type) {
+            this->type = type;
+        }
+    
+        Color24 Li(Ray& ray, const Scene * scene, Color24 bkg_color) const {
+            
+            Color24 color_ = bkg_color;
+            auto obj_list_ = scene->obj_list;
+
+            Surfel sf;
+            for(int k = 0; k < obj_list_.size(); k++){
+                if(obj_list_[k]->intersect(ray, &sf)){
+                    //sf.primitive->get_material()->kd();
+                    Vec normal = unique_vector(sf.n);
+                    Vec c(normal.v1+1, normal.v2+1, normal.v3 +1, 0);
+                    c = c * 0.5;
+                    //c.print();
+                    color_ = c;
+                }
+            }
+
+            return color_;
+        }
+        virtual ~NormalIntegrator() = default;
+};
+
 #endif
