96633aed10115749cd0180c35055eba4f4883413
Henrique David <henriquemed101@gmail.com>
2020-10-31 15:01:59 -0300

    correçao

--- include/light/spot.hpp
+++ include/light/spot.hpp
@@ -5,7 +5,7 @@
 #include "../vec.hpp"
 #include "./light.hpp"
 #include "../scene.hpp"
-
+#include "math.h"
 
 class SpotLight : public Light{
     public:
@@ -14,21 +14,36 @@ class SpotLight : public Light{
     Vec scale;
     Vec from;
     Vec to;
-    Vec cutoff;
-    Vec falloff;
+    float cutoff;
+    float falloff;
 
-    SpotLight(std::string type, Vec i, Vec scale, Vec from, Vec to, Vec cutoff, Vec falloff) : Light(type){
+    SpotLight(std::string type, Vec i, Vec scale, Vec from, Vec to, float cutoff, float falloff) : Light(type){
         this->i = i;
         this->scale = scale;
         this->from = from;
         this->to = to;
-        this->cutoff = cutoff;
-        this->falloff = falloff;
+        this->cutoff = cos(((cutoff*M_PI)/180.0)/2.0);
+        this->falloff = cos(((falloff*M_PI)/180.0)/2.0);
     }
 
-    Vec sample_Li(const Surfel& hit /*in*/, Vec v, Vec *wi) {}
     void preprocessLight(Scene & scene){
+    }
 
+    Vec sample_Li(const Surfel& hit /*in*/, Vec v, Vec *wi) {
+
+        Vec lightDir = from - hit.p;
+        lightDir = normalize(lightDir);
+
+        Vec spotDir = to - from;
+        spotDir = normalize(-spotDir);
+
+        float theta = dot(lightDir, spotDir);///(magnitude(lightDir)*magnitude(spotDir));
+        
+        float epsilon   = falloff - cutoff;
+        float intensity = std::min(1.0f, std::max(0.f, (theta - cutoff) / epsilon));
+        
+        *wi = i * intensity;
+        return spotDir;
     }
 };
 
